431085cb3a835c674024c2483d08bcc216569444
zer0 <ffelipemagallhaes@gmail.com>
2025-07-31 22:34:22 -0400

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello world. This is V0.0
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
