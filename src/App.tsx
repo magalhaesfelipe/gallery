eba286fe80becf4360be5656a0d223243563b080
zer0 <ffelipemagallhaes@gmail.com>
2025-07-31 23:34:13 -0400

    feat: implement get advice app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState<string>();
+
+  const getSomeAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  };
+
+  useEffect(() => {
+    getSomeAdvice();
+  }, []);
+
   return (
-    <div>
-      Hello world. This is V0.0
-      <div></div>
+    <div className="bg-purple-900 flex gap-y-12 flex-col  w-full h-full items-center justify-center">
+      <h1 className="text-4xl font-bold">{advice}</h1>
+      <button
+        onClick={getSomeAdvice}
+        className="bg-purple-700 cursor-pointer p-5 rounded-sm font-semibold text-amber-50"
+      >
+        get advice
+      </button>
     </div>
   );
 }
